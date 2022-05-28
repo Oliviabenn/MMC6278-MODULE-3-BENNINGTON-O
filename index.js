ee482c5359a3958942b1a081384416d38d759cfd
Olivia <oliviabennington@gmail.com>
2022-05-28 00:16:10 -0400

    questions added to array

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 //pseudocode
-
-var questionsArr = [
-      {
-        question: 'Canada is the second-largest country in the world.',
-        answer: true
-      }
-]
-
 function runQuiz(){
 
+var questionsArr = [
+//1
+      {
+        question: 'Canada is the second-largest country in the world.',
+        answer: "true"
+      },
+//2
+      {
+        question: 'Canada is the second-largest country in the world.',
+        answer: "true"
+      },
+//3
+      {
+        question: 'Canada is the second-largest country in the world.',
+        answer: "true"
+      },
+//4
+      {
+        question: 'Canada is the second-largest country in the world.',
+        answer: "true"
+      },
+//5
+      {
+        question: 'Canada is the second-largest country in the world.',
+        answer: "true"
+      },
+//6
+      {
+        question: 'Canada is the second-largest country in the world.',
+        answer: "true"
+      },
+]
+var score = 0;
 }
