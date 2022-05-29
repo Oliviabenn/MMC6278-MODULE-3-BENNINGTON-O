e26e96d9d5e5e25eb1e8249ccc11df2d31784716
Olivia <oliviabennington@gmail.com>
2022-05-29 14:31:50 -0400

    took booelan values out of quote

--- index.js
+++ index.js
@@ -5,32 +5,32 @@ var questionsArr = [
 //1
       {
         question: 'Canada is the second-largest country in the world.',
-        answer: "true"
+        answer: true
       },
 //2
       {
         question: 'An element is a pure substance that contains only one type of atom.',
-        answer: "true"
+        answer: true
       },
 //3
       {
         question: 'Mount Everest is present in the Himalayas.',
-        answer: "true"
+        answer: true
       },
 //4
       {
         question: 'Scapula is also called a shoulder bone.',
-        answer: "true"
+        answer: true
       },
 //5
       {
         question: 'The Nile is the longest river in Africa.',
-        answer: "true"
+        answer: true
       },
 //6
       {
         question: 'All of these questions have been truths.',
-        answer: "true"
+        answer: true
       }
 ];
 
